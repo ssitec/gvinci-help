fb35ee312530738b6b75feaf770f9b953331ae73
Silvano Matta <silvano@ssitecnologia.com.br>
2025-07-26 13:42:30 -0400

    pt-BR

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,8 +32,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'br',
-    locales: ['pt'],
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR'],
   },
 
   presets: [
