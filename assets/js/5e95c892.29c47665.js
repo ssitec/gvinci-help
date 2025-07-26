943e1c3daa0e25338b852aee1e8b01d923c22016
Silvano Matta <silvano@ssitecnologia.com.br>
2025-07-26 17:22:26 -0400

    Deploy site

---
+++ assets/js/5e95c892.29c47665.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgvinci_help=self.webpackChunkgvinci_help||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>h});r(6540);var c=r(4164),n=r(7559),u=r(5500),a=r(2831),i=r(1656),l=r(4848);function h(e){return(0,l.jsx)(u.e3,{className:(0,c.A)(n.G.wrapper.docsPages),children:(0,l.jsx)(i.A,{children:(0,a.v)(e.route.routes)})})}}}]);
---
+++ assets/js/a7456010.23e804b0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgvinci_help=self.webpackChunkgvinci_help||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
